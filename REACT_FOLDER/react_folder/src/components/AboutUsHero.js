ff6cade9a30fdcf7ada0816ad9026c3cf77fb9eb
Timothy Kim <timothy.e.kim@sjsu.edu>
2023-12-03 01:28:29 -0800

    Couple of test cases

--- REACT_FOLDER/react_folder/src/components/AboutUsHero.js
+++ REACT_FOLDER/react_folder/src/components/AboutUsHero.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import '../App.css';
 import { Button } from './Button';
 import './AboutUsHero.css';
 function AboutUsHero() {
   return (
     <div className = 'about-us-section'>
       <h1>About Us</h1>
-      <img src = "images/Staff Christmas 2022.JPG"></img>
+      <img src = "images/Staff Christmas 2022.JPG" className='about-us-bg-img' data-testid='about-us-bg-img'></img>
       <p>Information about the city and what it has to offer!</p>
     </div>
   );
