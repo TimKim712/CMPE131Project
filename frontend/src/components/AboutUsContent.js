653e4b5931d418cac4ceb4345fd83070e36f6706
Arpita Misal <arpitamisal2003@gmail.com>
2023-12-05 00:04:03 -0800

    final_commit

--- frontend/src/components/AboutUsContent.js
+++ frontend/src/components/AboutUsContent.js
@@ -14,7 +14,7 @@ function aboutUsContent() {
                         and convenient overnight stop for those traveling from Detroit to Michigan. The town was officially recognized
                         as a city on April 1, 1945, and a city charter was adopted in 1963. The charter is still in use to this day.
                        </p>
-                       <div className='image-container'>
+                       <div className='image-container' id='hover'>
                     <img src='images/5D6A8021_A9F4696E-5056-A36A-06AF9124C49D67C6-a9f4581f5056a36_a9f46d3f-5056-a36a-06a2a32b691b81b0.jpg' alt='City History' className='photo1'></img>
                     <div className="caption">Downtown Williamston</div>
                     </div>
@@ -30,7 +30,7 @@ function aboutUsContent() {
                         Wheatfield Valley Golf Course, as well as two theaters: the Williamston Theater and the Sun Theater. The city also provides
                         a wide range of shops ranging from clothing stores, restaurants, grocery stores, and special interest shops. The largest employer in the City is the Williamston Community Schools with 170 employees.
                     </p>
-                    <div className='image-container'>
+                    <div className='image-container' id='hover'>
                     <img src='images/Wmston_Mill_Stone.jpg' alt='City History' className='photo1'></img>
                     <div className="caption">Grindstone commemorating Williams' mill in the 1800s</div>
                     </div>
@@ -48,7 +48,7 @@ function aboutUsContent() {
                         Williamston also includes city departments such as the Police Department, the Department of Public Works, Assessing Department,
                         and Building Department.
                         </p>
-                        <div className='image-container'>
+                        <div className='image-container' id='hover'>
                         <img src='images/Williamston-City-Hall-1170x878.jpg' alt='City History' className='photo1'></img>
                         <div className="caption">City Hall of Williamston</div>
                         </div>
